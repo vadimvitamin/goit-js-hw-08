37a2af557ea09215926b7511309c75d9cbe7d57d
Vadym Kolybchuk <99030890+vadimvitamin@users.noreply.github.com>
2022-10-24 18:57:03 +0300

    lodash

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,22 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const localStorageKey = 'videoplayer-current-time';
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.setCurrentTime(loadPosition());
+const throtledOnTimeUpdate = throttle(onTimeUpdate, 1000);
+player.on('timeupdate', throtledOnTimeUpdate);
+
+function onTimeUpdate(e) {
+  savePosition(e.seconds);
+}
+
+function savePosition(value) {
+  localStorage.setItem(localStorageKey, String(value));
+}
+
+function loadPosition() {
+  return localStorage.getItem(localStorageKey) || 0;
+}
