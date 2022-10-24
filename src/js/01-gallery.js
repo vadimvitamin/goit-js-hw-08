00fa21271fd22b202656d4e65cf82f03d40b7c88
Vadym Kolybchuk <99030890+vadimvitamin@users.noreply.github.com>
2022-10-24 17:49:04 +0300

    add simple-lightbox.min.css

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,6 +1,27 @@
 // Add imports above this line
 import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items';
 // Change code below this line
 console.log(SimpleLightbox);
 console.log(galleryItems);
+const galleryRef = document.querySelector('.gallery');
+
+galleryRef.insertAdjacentHTML('beforeend', getGalleryListMarkup(galleryItems));
+const lightbox = new SimpleLightbox('.gallery__item', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+console.log(lightbox);
+
+function getGalleryItemMarkup({ preview, original, description }) {
+  return `
+  <a class="gallery__item" href="${original}">
+    <img class="gallery__image" src="${preview}" alt="${description}"/>
+  </a>
+  `;
+}
+
+function getGalleryListMarkup(array) {
+  return array.map(it => getGalleryItemMarkup(it)).join('');
+}
